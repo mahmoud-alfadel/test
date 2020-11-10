a5fe47478a3dcb9384b66f12d6b16abf54d024f8
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2020-11-10 06:16:19 +0600

    refactored and updated template.html

--- tests/test_helper_step1.py
+++ tests/test_helper_step1.py
@@ -3,7 +3,6 @@ from dependency_threat.helper.step1 import (
     get_package_dependencies,
     fetch_dependency_history,
 )
-from dependency_threat.dependency_threat import access_tokens
 import pandas as pd
 import os
 
@@ -13,7 +12,7 @@ path = os.path.abspath(__file__).rsplit("/", 1)[0]
 def test_fetch_dependency_history():
     test_df = pd.read_csv(path + "/sample_outputs/test_step1.csv")
     result_df = fetch_dependency_history(
-        "https://github.com/wasi0013/online-voting-system", access_tokens
+        "https://github.com/wasi0013/online-voting-system", []
     )
     assert len(result_df) == len(test_df)
     assert len(result_df.columns) == len(test_df.columns)
